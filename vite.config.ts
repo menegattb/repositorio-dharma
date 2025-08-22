28dfb1e82400704a46ef5e2900aa7614f1a8896d
Sarah86 <sarah.goncalves@gmail.com>
2025-08-23 01:48:31 +0200

    Removed unused proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,10 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export const HOSTNAME = "https://www.acaoparamita.com.br";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    proxy: {
-      "/api": {
-        target: HOSTNAME, // Your WordPress site URL
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean

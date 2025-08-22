016a1acad2abb3849c545097c6ad75ab23e768e3
sarah <sarah@hollyhealth.io>
2025-08-23 01:03:57 +0200

    Added login page

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,23 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
+export const HOSTNAME = "https://www.acaoparamita.com.br";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      "/api": {
+        target: HOSTNAME, // Your WordPress site URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
